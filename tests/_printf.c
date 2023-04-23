1e4ae33babc4dbcc10c8978f15384726b1c90d68
NacerEddine-19 <majidnacereddine@gmail.com>
2023-04-24 00:30:23 +0100

    testing

--- tests/_printf.c
+++ tests/_printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <limits.h>
 
 #define BUFF_SIZE 1024
 
@@ -106,6 +107,50 @@ int _printf(const char *format, ...)
                 count++;
                 break;
             }
+            case 'p':
+            {
+                void *arg = va_arg(args, void *);
+                unsigned long long ptr_val = (unsigned long long)arg;
+                char temp_buff[16];
+                int i = 0;
+
+                if (arg == NULL)
+                {
+                    buffer[buff_ind++] = '(';
+                    buffer[buff_ind++] = 'n';
+                    buffer[buff_ind++] = 'i';
+                    buffer[buff_ind++] = 'l';
+                    buffer[buff_ind++] = ')';
+                    if (buff_ind == BUFF_SIZE)
+                    {
+                        print_buffer(buffer, &buff_ind);
+                    }
+                    count += 5;
+                }
+                else
+                {
+                    buffer[buff_ind++] = '0';
+                    buffer[buff_ind++] = 'x';
+
+                    do
+                    {
+                        temp_buff[i++] = "0123456789abcdef"[ptr_val % 16];
+                        ptr_val /= 16;
+                    } while (ptr_val > 0);
+
+                    while (--i >= 0)
+                    {
+                        buffer[buff_ind++] = temp_buff[i];
+                        if (buff_ind == BUFF_SIZE)
+                        {
+                            print_buffer(buffer, &buff_ind);
+                        }
+                        count++;
+                    }
+                    count += 2; /* for the '0x' prefix */
+                }
+                break;
+            }
             default:
             {
                 break;
@@ -138,8 +183,36 @@ void print_buffer(char buffer[], int *buff_ind)
 
 int main()
 {
-    _printf("Hello, %s!\n", "world");
-    _printf("The answer is %d%%.\n", 42);
-    _printf("The character '%c' has ASCII code %d.\n", 'A', 'A');
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    // _printf("Unsigned:[%u]\n", ui);
+    // printf("Unsigned:[%u]\n", ui);
+    // _printf("Unsigned octal:[%o]\n", ui);
+    // printf("Unsigned octal:[%o]\n", ui);
+    // _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    // printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    // _printf("Address:[%p]\n", addr);
+    // printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    // _printf("Unknown:[%r]\n");
+    // printf("Unknown:[%r]\n");
     return 0;
 }
