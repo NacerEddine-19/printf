9747428911b584963b99f80d9c668e601bf29abf
NacerEddine-19 <majidnacereddine@gmail.com>
2023-04-27 00:13:03 +0100

    hexadecimal

--- main.h
+++ main.h
@@ -113,6 +113,8 @@ struct fmt
 						  int flags, int width, int precision, int size);
 	int print_hexa_upper(va_list types, char buffer[],
 						 int flags, int width, int precision, int size);
+	int print_hexa(va_list types, char map_to[],
+				   char buffer[], int flags, char flag_ch, int width, int precision, int size);
 	/* Functions to handle other specifiers */
 	int get_flags(
 		const char *format,
